823a01b28f2ca7cc373580e83ec01827d4b8f845
yannikschaelte <yannik.schaelte@gmail.com>
2018-05-28 17:13:05 +0200

    cont

--- enzymatic_catalysis/test_script.py
+++ enzymatic_catalysis/test_script.py
@@ -1,3 +1,17 @@
-import models
+from models import *
+import scipy as sp
+import numpy as np
+
+def cur_f(t, x):
+    return f(t, x, th_true['th0'], th_true['th1'], th_true['th2'], th_true['th3'])
+
+sol = sp.integrate.solve_ivp(fun=cur_f,
+                             t_span=(min(t), max(t)),
+                             y0=get_x0(),
+                             t_eval=t)
+
+
+
+print(sol)
 
 

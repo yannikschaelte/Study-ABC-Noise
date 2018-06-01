21201c72cca39f2f9492b07f1cb2411da5ccb1d1
yannikschaelte <yannik.schaelte@gmail.com>
2018-06-01 16:23:40 +0200

    rerun tests

--- conversion_reaction/test3.py
+++ conversion_reaction/test3.py
@@ -14,21 +14,38 @@ db_path = "sqlite:///db3.db"
 
 # ACCEPTOR
 
-distr = stats.multivariate_normal([0, 0], noise**2 * np.asarray([[1, 0],[0, 1]]))
-acceptor = pyabc.StochasticAcceptor(distr)
+distr = stats.multivariate_normal(np.zeros(n_timepoints), noise**2 * np.eye(n_timepoints))
+acceptor = pyabc.StochasticAcceptor(distr=distr)
 
-# PERFORM ABC ANALYSIS
+# PERFORM ABC ANALYSIS WITH 0,1-THRESHOLD AS PREPARATION
+
+max_nr_populations = max_nr_populations - 1
 
 abc = pyabc.ABCSMC(models=model,
                    parameter_priors=prior,
-                   distance_function=pyabc.distance_functions.NoDistance,
+                   distance_function=distance,
+                   populations_size=pop_size,
+                   transitions=transition,
+                   eps=eps,
+                   acceptor=acceptor,
+                   sampler=sampler)
+
+abc.run(minimum_epsilon=0, max_nr_populations=max_nr_populations)
+
+# PERFORM ABC ANALYSIS WITH STOCHASTIC ACCEPTOR
+
+abc = pyabc.ABCSMC(models=model,
+                   parameter_priors=prior,
+                   distance_function=pyabc.distance_functions.NoDistance(),
                    population_size=pop_size,
                    transitions=transition,
-                   eps=pyabc.epsilon.NoEpsilon)
+                   eps=pyabc.epsilon.NoEpsilon(),
+                   acceptor=acceptor,
+                   sampler=sampler)
 
-abc.new(db_path, get_y_meas())
+abc.load(db_path)
 
-h = abc.run(minimum_epsilon=0, max_nr_populations=max_nr_populations)
+h = abc.run(minimum_epsilon=0, max_nr_populations=1)
 
 # PLOT
 

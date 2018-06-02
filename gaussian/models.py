6e3926952be6c7b5a1c42e2cd712e2b563e32e05
yannikschaelte <yannik.schaelte@gmail.com>
2018-06-02 09:30:51 +0200

    gaussian: added multiple tests w, w/o var data and sim

--- gaussian/models.py
+++ gaussian/models.py
@@ -16,10 +16,12 @@ noise = 1
 noise_model = np.random.randn
 
 # prior
-prior_lb = -5
+prior_lb = 0
 prior_ub = 5
 prior = pyabc.Distribution(**{key: pyabc.RV('uniform', prior_lb, prior_ub - prior_lb)
                               for key in ['th0']})
+prior_uvar = pyabc.Distribution(**{key: pyabc.RV('uniform', prior_lb, prior_ub - prior_lb)
+                                   for key in ['th0', 'th1']})
 
 # MODEL
 
@@ -32,9 +34,31 @@ def model_random(p):
     return {'y0': p['th0'] + noise * noise_model()}
 
 
+def model_random_uvar(p):
+    return {'y0': p['th0'] + p['th1'] * noise_model()}
+
+
+def model2(p):
+    """
+    Model2 returns a the sample mean and standard deviation.
+    """
+    xs = np.asarray([p['th0'] for _ in range(10)])
+    return {'y0': np.mean(xs), 'y1': np.std(xs)}
+
+
+def model2_random(p):
+    xs = np.asarray([p['th0'] + noise * noise_model() for _ in range(10)])
+    return {'y0': np.mean(xs), 'y1': np.std(xs)}
+
+
+def model2_random_uvar(p):
+    xs = np.asarray([p['th0'] + p['th1'] * noise_model() for _ in range(10)])
+    return {'y0': np.mean(xs), 'y1': np.std(xs)}
+
+
 # TRUE VARIABLES
 
-th0_true = 0 # mean
+th0_true = 2.5 # mean
 th_true = {'th0': th0_true}
 y_true = model(th_true)
 
@@ -42,7 +66,7 @@ y_true = model(th_true)
 th1_true = 1 # standard deviation
 th_true_uvar = {'th0': th0_true, 'th1': th1_true}
 
-# MESAURED DATA
+# MEASURED DATA
 
 _y_meas = None
 
@@ -58,6 +82,22 @@ def get_y_meas():
     except Exception:
         y_true = model(th_true)
         y_meas = {'y0': y_true['y0'] + noise * noise_model()}
+        pickle.dump(y_meas, open(y_meas_file, 'wb'))
+
+    _y_meas = y_meas
+    return y_meas
+
+
+def get_y_meas2():
+    global _y_meas
+    if _y_meas is not None:
+        return _y_meas
+
+    y_meas_file = "y_meas2.dat"
+    try:
+        y_meas = picle.load(open(y_meas_file, 'rb'))
+    except Exception:
+        y_meas = model2_random(th_true)
         pickle.dump(y_meas, open(y_meas_file, 'wb'))
 
     _y_meas = y_meas
@@ -83,4 +123,15 @@ def visualize(label, history):
                                           xmin=prior_lb, xmax=prior_ub, numx=300)
     ax.axvline(th0_true, color='k', linestyle='dashed')
     plt.savefig(label + "_kde_1d_" + str(t))
+    plt.close()
+
+
+def visualize_uvar(label, history):
+    t = history.max_t
+
+    df, w = history.get_distribution(m=0, t=t)
+    ax = pyabc.visualization.plot_kde_matrix(df, w,
+            limits={key: (prior_lb, prior_ub)
+                    for key in ['th0', 'th1']})
+    plt.savefig(label + "_kde_2d_" + str(t))
     plt.close()
---
+++ gaussian/test5.py
@@ -0,0 +1,23 @@
+import pyabc
+from models import *
+
+# VARIABLEs
+
+db_path = "sqlite:///db5.db"
+
+# PERFORM ABC ANALYSIS
+
+abc = pyabc.ABCSMC(models=model2_random_uvar,
+                   parameter_priors=prior_uvar,
+                   distance_function=distance,
+                   population_size=pop_size,
+                   transitions=transition,
+                   eps=eps)
+
+abc.new(db_path, get_y_meas2())
+
+h = abc.run(minimum_epsilon=0, max_nr_populations=max_nr_populations)
+
+# PLOT
+
+visualize_uvar("test5", h)

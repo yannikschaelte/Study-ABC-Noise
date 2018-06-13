be232e68cb81681c39c8eb3b38d10e06453faded
yannikschaelte <yannik.schaelte@gmail.com>
2018-06-13 08:39:24 +0200

    tidy up

--- errormodel.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-import numpy as np
-
-x_axis = np.arange(-5, 5, 0.01)
-plt.plot(x_axis, stats.norm.pdf(x_axis, 0, 1))
-plt.savefig("normal_error")

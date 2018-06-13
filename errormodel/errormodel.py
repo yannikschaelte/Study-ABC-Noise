d671e8a86f6f8fe4ccc9754479b9193feaefa3af
yannikschaelte <yannik.schaelte@gmail.com>
2018-06-13 08:36:18 +0200

    create errormodel plots in own dir

---
+++ errormodel/errormodel.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+
+plt.figure()
+x_axis = np.arange(-5, 5, 0.01)
+plt.plot(x_axis, stats.norm.pdf(x_axis, 0, 1))
+plt.savefig("normal_error")
+
+plt.figure()
+a=np.sqrt(3)
+plt.step([-5, -a, a, 5], [0, 1/(2*a), 0, 0], where='post')
+plt.savefig("step_error")

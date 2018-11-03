114435508c28930deca336584c082b1ca29f3a68
yannikschaelte <yannik.schaelte@gmail.com>
2018-11-03 17:19:09 +0100

    for copy

---
+++ simple_mjp/test_p2.py
@@ -0,0 +1,16 @@
+from model import *
+import pyabc
+
+db_file= "sqlite:///db.db"
+
+abc = pyabc.ABCSMC(models=model,
+                   parameter_priors=prior,
+                   distance_function=distance,
+                   summary_statistics=sumstat_p,
+                   population_size=pop_size,
+                   sampler=sampler)
+abc.new(db=db_file, observed_sum_stat=sumstat_p_obs)
+h = abc.run(minimum_epsilon=0, max_nr_populations=max_nr_populations)
+#h = pyabc.History(db_file)
+#h.id = 1
+visualize("viz_" + str(n_r), h)

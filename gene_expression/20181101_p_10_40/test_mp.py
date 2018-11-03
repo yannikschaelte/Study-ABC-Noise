9135aafcee2d8394d901d64648559cc01293c0c1
yannikschaelte <yannik.schaelte@gmail.com>
2018-11-03 13:54:37 +0100

    try with mp

--- gene_expression/20181101_p_10_40/test_mp.py
+++ gene_expression/20181101_p_10_40/test_mp.py
@@ -1,17 +1,18 @@
 from model import *
 import pyabc
 
-db_file= "sqlite:///db.db"
+db_file = "sqlite:///db_mp.db"
 
 abc = pyabc.ABCSMC(models=model,
                    parameter_priors=prior,
                    distance_function=distance,
-                   summary_statistics=sumstat_p,
+                   summary_statistics=sumstat_mp,
                    population_size=pop_size,
                    sampler=sampler)
+abc.new(db=db_file, observed_sum_stat=sumstat_mp_obs)
 
-abc.new(db=db_file, observed_sum_stat=sumstat_p_obs)
 h = abc.run(minimum_epsilon=0, max_nr_populations=max_nr_populations)
 
 h = pyabc.History(db_file)
-visualize("viz_" + str(n_r), h)
+visualize("viz_mp_" + str(n_r), h)
+

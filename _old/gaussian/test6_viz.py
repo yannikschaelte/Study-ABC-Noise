61cc20d53451b31dbc133f00fcfe56d7820585ad
yannikschaelte <yannik.schaelte@gmail.com>
2019-01-20 11:05:03 +0100

    add test7 for adaptive ABC-SMC

--- _old/gaussian/test6_viz.py
+++ _old/gaussian/test6_viz.py
@@ -9,14 +9,22 @@ h = pyabc.History("sqlite:///db6.db")
 h.id = 1
 s2 = np.asarray(h.get_all_populations()['samples'][1:])
 s1 = np.zeros_like(s2)
+
 h.id = 2
 s1[0] = h.get_all_populations()['samples'][1]
+
+h = pyabc.History("sqlite:///db7.db")
+h.id = 1
+s3 = np.zeros_like(s2)
+tmp = np.asarray(h.get_all_populations()['samples'][1:])
+s3[:len(tmp)] = tmp
+
 l = len(s2)
-print(s1, s2)
+print(s1, s2, s3)
 for j in range(l):
-    plt.bar(np.arange(2),(s1[j], s2[j]),
-            bottom=(sum(s1[k] for k in range(j)), sum(s2[k] for k in range(j))))
-plt.xticks(np.arange(2),("Rejection ABC", "ABC-SMC"))
+    plt.bar(np.arange(3),(s1[j], s2[j], s3[j]),
+            bottom=(sum(s1[k] for k in range(j)), sum(s2[k] for k in range(j)), sum(s3[k] for k in range(j))))
+plt.xticks(np.arange(3),("Rejection ABC", "ABC-SMC", "Adaptive ABC-SMC"))
 plt.title("Total required samples")
 plt.ylabel("Samples")
 plt.xlabel("Method")
---
+++ _old/gaussian/test7.py
@@ -0,0 +1,38 @@
+"""
+This is for testing the stochastic acceptor
+with known variance.
+
+Attention:
+    Set models.noise = 0.01 to get nicer results.
+"""
+
+import pyabc
+from models import *
+from scipy import stats
+
+# VARIABLEs
+
+db_path = "sqlite:///db7.db"
+distr = stats.multivariate_normal([0], [noise**2])
+nr_pops = 8
+def pdf(x_0, x):
+    return stats.multivariate_normal.pdf(np.array(list(x_0.values())) - np.array(list(x.values())), mean=[0], cov=[noise**2])
+acceptor = pyabc.StochasticAcceptor(pdf=pdf)
+
+# PERFORM ABC ANALYSIS
+
+abc = pyabc.ABCSMC(models=model,
+                   parameter_priors=prior_uvar,
+                   distance_function=pyabc.NoDistance(),
+                   population_size=100,
+                   transitions=transition,
+                   eps=pyabc.NoEpsilon(),
+                   acceptor=acceptor)
+
+abc.new(db_path, get_y_meas())
+
+h = abc.run(minimum_epsilon=1, max_nr_populations=nr_pops)
+
+# PLOT
+
+visualize("test7", h)

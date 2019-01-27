bcb37726c7a00d678d05c7d7bf3a4cb9f50b09bb
yannikschaelte <yannik.schaelte@gmail.com>
2019-01-27 18:26:36 +0100

    update stacks viz script

--- _old/conversion_reaction/viz_stacks.py
+++ _old/conversion_reaction/viz_stacks.py
@@ -5,44 +5,27 @@ import matplotlib
 from models import *
 
 ss = []
-n_tests = 5
-labels = ["Adaptive", ""]
-for _ in range(4):
+n_tests = 6
+labels = ["acc-dec", "dec", "exp", "daly", "all", "all-bestc"]
+for _ in range(n_tests):
     ss.append(np.zeros(max_nr_populations))
 
 h = pyabc.History("sqlite:///db5.db")
+for i in range(n_tests - 1):
+    h.id=i + 1
+    s = np.asarray(h.get_all_populations()['samples'][1:])
+    ss[i][:len(s)] = s
 
+h = pyabc.History("sqlite:///db6.db")
+s = np.asarray(h.get_all_populations()['samples'][1:])
+ss[5][:len(s)] = s
 
-h.id = 1
-s2 = np.asarray(h.get_all_populations()['samples'][1:])
+print(ss)
 
-s1 = np.zeros_like(s2)
-s3 = np.zeros_like(s2)
-s4 = np.zeros_like(s2)
-s5 = np.zeros_like(s2)
-
-h.id = 1
-s1[0] = h.get_all_populations()['samples'][1]
-
-h.id = 3
-tmp = np.asarray(h.get_all_populations()['samples'][1:])
-s3[:len(tmp)] = tmp
-
-h.id = 4
-tmp = np.asarray(h.get_all_populations()['samples'][1:])
-s4[:len(tmp)] = tmp
-
-h = pyabc.History("sqlite:///db7.db")
-h.id = 1
-tmp = np.asarray(h.get_all_populations()['samples'][1:])
-s5[:len(tmp)] = tmp
-
-l = len(s2)
-print(s1, s2, s3, s4, s5)
-for j in range(l):
-    plt.bar(np.arange(5),(s1[j], s2[j], s3[j], s4[j], s5[j]),
-            bottom=(sum(s1[k] for k in range(j)), sum(s2[k] for k in range(j)), sum(s3[k] for k in range(j)), sum(s4[k] for k in range(j)), sum(s5[k] for k in range(j))))
-plt.xticks(np.arange(5),("Rejection ABC", "Decay ABC-SMC", "Daly ABC-SMC", "Exponential Decay ABC-SMC", "Adaptive ABC-SMC"),rotation='vertical')
+for j in range(max_nr_populations):
+    plt.bar(np.arange(n_tests),tuple([ss[p][j] for p in range(n_tests)]),
+            bottom=(tuple([sum(ss[p][k] for k in range(j)) for p in range(n_tests)])))
+plt.xticks(np.arange(n_tests), labels, rotation='vertical')
 plt.title("Total required samples")
 plt.ylabel("Samples")
 plt.xlabel("Method")

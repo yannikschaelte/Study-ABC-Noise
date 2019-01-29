0d74c7df3c40841465ac655e813ce872a66e03dc
yannikschaelte <yannik.schaelte@gmail.com>
2019-01-29 08:57:59 +0100

    modify stacks

--- _old/conversion_reaction/viz_stacks.py
+++ _old/conversion_reaction/viz_stacks.py
@@ -6,45 +6,25 @@ from models import *
 
 ss = []
 n_tests = 5
-labels = ["Adaptive", ""]
-for _ in range(4):
+labels = ["acc + decay", "decay", "exp_decay", "daly", "all"]
+for _ in range(n_tests):
     ss.append(np.zeros(max_nr_populations))
 
 h = pyabc.History("sqlite:///db5.db")
 
+for j in range(n_tests):
+    h.id = j + 1
+    s = np.asarray(h.get_all_populations()['samples'][1:])
+    ss[j][:len(s)] = s
 
-h.id = 1
-s2 = np.asarray(h.get_all_populations()['samples'][1:])
+print(ss)
+print([sum(ss[j]) for j in range(n_tests)])
 
-s1 = np.zeros_like(s2)
-s3 = np.zeros_like(s2)
-s4 = np.zeros_like(s2)
-s5 = np.zeros_like(s2)
-
-h.id = 1
-s1[0] = h.get_all_populations()['samples'][1]
-
-h.id = 3
-tmp = np.asarray(h.get_all_populations()['samples'][1:])
-s3[:len(tmp)] = tmp
-
-h.id = 4
-tmp = np.asarray(h.get_all_populations()['samples'][1:])
-s4[:len(tmp)] = tmp
-
-h = pyabc.History("sqlite:///db7.db")
-h.id = 1
-tmp = np.asarray(h.get_all_populations()['samples'][1:])
-s5[:len(tmp)] = tmp
-
-l = len(s2)
-print(s1, s2, s3, s4, s5)
-for j in range(l):
-    plt.bar(np.arange(5),(s1[j], s2[j], s3[j], s4[j], s5[j]),
-            bottom=(sum(s1[k] for k in range(j)), sum(s2[k] for k in range(j)), sum(s3[k] for k in range(j)), sum(s4[k] for k in range(j)), sum(s5[k] for k in range(j))))
-plt.xticks(np.arange(5),("Rejection ABC", "Decay ABC-SMC", "Daly ABC-SMC", "Exponential Decay ABC-SMC", "Adaptive ABC-SMC"),rotation='vertical')
-plt.title("Total required samples")
-plt.ylabel("Samples")
+for k in range(max_nr_populations):
+    plt.bar(np.arange(n_tests), tuple([ss[j][k] for j in range(n_tests)]),
+            bottom=tuple([sum(ss[j][l] for l in range(k)) for j in range(n_tests)]))
+plt.xticks(np.arange(n_tests), labels)
+plt.title("total required samples")
 plt.xlabel("Method")
-plt.gcf().tight_layout()
-plt.savefig("test6_bar")
+plt.ylabel("Samples")
+plt.savefig("total_samples.png")

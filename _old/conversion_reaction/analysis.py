dc7ac53738dd277e7dc46869a3143c8b4baa3bf3
yannikschaelte <yannik.schaelte@gmail.com>
2019-01-29 07:24:42 +0100

    save theoretical acceptance rates

--- _old/conversion_reaction/analysis.py
+++ _old/conversion_reaction/analysis.py
@@ -1,11 +1,12 @@
 import numpy as np
 import scipy as sp
+import pickle
 from models import *
 
 
 print(y_obs)
 
-def integrand_from_prior(th):
+def integrand_from_prior(th, c):
     if type(th) is list:
         th = {'th0': th[0], 'th1': th[1]}
 
@@ -18,14 +19,18 @@ def integrand_from_prior(th):
     # likelihood
     likelihood_val = normal_dty(y_obs, y, sigma)
 
-    # c
-    c = normal_dty(y_obs, y_obs, sigma)
-
     # prior
     prior_val = prior.pdf(th)
 
     return (likelihood_val / c) * prior_val
 
+
+c_max = normal_dty(get_y_obs()['y'].flatten(), get_y_obs()['y'].flatten(), noise * np.ones(n_timepoints))
+def integrand_from_prior_c_max(th):
+    return integrand_from_prior(th, c_max)
+c_found = pickle.load(open("best_found_pdf_" + str(noise) + "_" + str(n_timepoints) + ".dat", 'rb'))
+def integrand_from_prior_c_found(th):
+    return integrand_from_prior(th, c_found)
 
 def true_posterior_unscaled(th):
     if type(th) is list:
@@ -56,7 +61,7 @@ def true_posterior(th):
     return true_posterior_unscaled(th) / true_posterior_normalization
 
 
-def integrand_from_posterior(th):
+def integrand_from_posterior(th, c):
     if type(th) is list:
         th = {'th0': th[0], 'th1': th[1]}
 
@@ -69,19 +74,25 @@ def integrand_from_posterior(th):
     # likelihood
     likelihood_val = normal_dty(y_obs, y, sigma)
 
-    # c
-    c = normal_dty(y_obs, y_obs, sigma)
-
     # posterior
     posterior_val = true_posterior(th)
 
     return (likelihood_val / c)  * posterior_val
 
-
-integral_prior= sp.integrate.dblquad(lambda x, y: integrand_from_prior([x,y]), limits['th0'][0], limits['th0'][1], lambda x: limits['th1'][0], lambda x: limits['th1'][1])[0]
-integral_posterior = sp.integrate.dblquad(lambda x, y: integrand_from_posterior([x,y]), limits['th0'][0], limits['th0'][1], lambda x: limits['th1'][0], lambda x: limits['th1'][1])[0]
-
-print(integral_prior, integral_posterior)
-
+def integrand_from_posterior_c_max(th):
+    return integrand_from_posterior(th, c_max)
+def integrand_from_posterior_c_found(th):
+    return integrand_from_posterior(th, c_found)
 
 
+integral_prior_max = sp.integrate.dblquad(lambda x, y: integrand_from_prior_c_max([x,y]), limits['th0'][0], limits['th0'][1], lambda x: limits['th1'][0], lambda x: limits['th1'][1])[0]
+integral_posterior_max = sp.integrate.dblquad(lambda x, y: integrand_from_posterior_c_max([x,y]), limits['th0'][0], limits['th0'][1], lambda x: limits['th1'][0], lambda x: limits['th1'][1])[0]
+
+
+integral_prior_found = sp.integrate.dblquad(lambda x, y: integrand_from_prior_c_found([x,y]), limits['th0'][0], limits['th0'][1], lambda x: limits['th1'][0], lambda x: limits['th1'][1])[0]
+integral_posterior_found = sp.integrate.dblquad(lambda x, y: integrand_from_posterior_c_found([x,y]), limits['th0'][0], limits['th0'][1], lambda x: limits['th1'][0], lambda x: limits['th1'][1])[0]
+
+print("prior_c_max, posterior_c_max, prior_c_found, posterior_c_found", integral_prior_max, integral_posterior_max, integral_prior_found, integral_posterior_found)
+
+
+pickle.dump((integral_prior_max, integral_posterior_max, integral_prior_found, integral_posterior_found), open("analysis.dat", 'wb'))

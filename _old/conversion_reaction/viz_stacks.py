f9122354e071e974739d1becbce02ecf12253c74
yannikschaelte <yannik.schaelte@gmail.com>
2019-02-06 16:38:17 +0100

    save results for 002 / 10 for results subdir

--- _old/conversion_reaction/viz_stacks.py
+++ _old/conversion_reaction/viz_stacks.py
@@ -5,24 +5,31 @@ import matplotlib
 from models import *
 
 ss = []
-n_tests = 10
+n_tests = 15
 labels = ["acc-dec", "dec", "exp", "daly", "all",
-          "acc-dec-c-opt", "dec-c-opt", "exp-c-opt", "daly-c-opt", "all-c-opt"]
+          "acc-dec-c-opt", "dec-c-opt", "exp-c-opt", "daly-c-opt", "all-c-opt",
+          "acc_dep_c-learned", "dec-c-learned", "exp-c-learned", "daly-c-learned", "all-c-learned"]
 
 for _ in range(n_tests):
     ss.append(np.zeros(max_nr_populations))
 
 h = pyabc.History("sqlite:///db5.db")
-for i in range(int(n_tests / 2)):
+for i in range(int(n_tests / 3)):
     h.id = i + 1
     s = np.asarray(h.get_all_populations()['samples'][1:])
     ss[i][:len(s)] = s
 
 h = pyabc.History("sqlite:///db6.db")
-for i in range(int(n_tests / 2)):
+for i in range(int(n_tests / 3)):
     h.id = i + 1
     s = np.asarray(h.get_all_populations()['samples'][1:])
-    ss[int(n_tests / 2 + i)][:len(s)] = s
+    ss[int(n_tests / 3 + i)][:len(s)] = s
+
+h = pyabc.History("sqlite:///db7.db")
+for i in range(int(n_tests / 3)):
+    h.id = i + 1
+    s = np.asarray(h.get_all_populations()['samples'][1:])
+    ss[int(n_tests / 3 * 2 + i)][:len(s)] = s
 
 print(ss)
 

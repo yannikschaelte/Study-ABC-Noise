5094ddf8472cddadf990a83ffc889650e5844329
yannikschaelte <yannik.schaelte@gmail.com>
2019-03-23 14:52:27 +0100

    test new pyabc viz routines

--- _old/conversion_reaction/viz_meta.py
+++ _old/conversion_reaction/viz_meta.py
@@ -7,6 +7,8 @@ list_label = ["Noisy model", "Deterministic model", "Stochastic acceptor", "Adap
 list_h = [pyabc.History("sqlite:///db1.db"), pyabc.History("sqlite:///db2.db"), pyabc.History("sqlite:///db3.db"), pyabc.History("sqlite:///db5.db")]
 
 
+pyabc.visualization.plot_confidence_intervals(list_h[0], confidences=[0.5, 0.75, 0.9, 0.95])
+plt.savefig("confidences.png")
 pyabc.visualization.plot_sample_numbers(list_h, list_label, rotation=45)
 plt.savefig("samples.png")
 pyabc.visualization.plot_epsilons(list_h, list_label)

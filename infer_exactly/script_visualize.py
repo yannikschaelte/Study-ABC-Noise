30da0e88fbbf0b0034fbdde0e88b14de721bfc38
yannikschaelte <yannik.schaelte@gmail.com>
2019-04-10 13:29:12 +0200

    plot true vals in kde

--- infer_exactly/script_visualize.py
+++ infer_exactly/script_visualize.py
@@ -1,28 +1,43 @@
 import pyabc
 import os
+import matplotlib.pyplot as plt
 
-db_files = [f for f in os.listdir('.') if isfile(f) and "db_" in f]
+
+db_files = [f for f in os.listdir('.') if os.path.isfile(f) and "db_" in f]
 if not db_files:
     raise ValueError("No database found.")
 db_file = "sqlite:///" + db_files[-1]
-print(f"Taking db file {db_file}")
+print(f"Using db file {db_file}")
+
 
 histories = []
 labels = []
 h = pyabc.History(db_file)
-h.id = 0
+h.id = 1
 histories.append(h)
 labels.append("Deterministic model + uniform acceptor")
 
 h = pyabc.History(db_file)
-h.id = 1
+h.id = 2
 histories.append(h)
 labels.append("Noisy model + uniform acceptor")
 
 h = pyabc.History(db_file)
-h.id = 2
+h.id = 3
 histories.append(h)
 labels.append("Deterministic model + stochastic acceptor")
 
 
-pyabc.visualize.plot_sample_numbers(histories, labels)
+gt_par = h.get_population(t=-1).get_list()[0].parameter
+
+
+pyabc.visualization.plot_sample_numbers(histories, labels)
+plt.savefig("samples.png")
+pyabc.visualization.plot_epsilons(histories, labels)
+plt.savefig("epsilons.png")
+for i in range(len(histories)):
+    pyabc.visualization.plot_histogram_matrix(histories[i])
+    plt.savefig("hist_" + str(i + 1) + ".png")
+    df, w = histories[i].get_distribution()
+    pyabc.visualization.plot_kde_matrix(df, w, refval=gt_par)
+    plt.savefig("kde_" + str(i + 1) + ".png")

b1eed0949510653a9bc35208a2979e6eb98bcba3
yannikschaelte <yannik.schaelte@gmail.com>
2019-04-16 14:54:09 +0200

    add infer_exactly_mrna and little test

---
+++ study_abc_noise/infer_exactly_mrna/script_run.py
@@ -0,0 +1,44 @@
+"""
+Illustrate the problem arising when not accounting for measurement noise.
+Possible test cases: A deterministic model (-> ML estimator weighted by the
+prior), and a simple SSA model (-> overestimation of parameters promoting
+noise).
+
+This means using a non-noisy model together with a deterministic acceptor,
+and comparing that to the true analysis performed using either a noisy model,
+or a stochastic acceptor.
+"""
+
+import pyabc
+import sys
+from study_abc_noise.model import MRNATranscriptionModelVars as ModelVars
+from study_abc_noise.util import create_sampler, get_timestamp
+from study_abc_noise.vars import AnalysisVars, Task
+
+
+mv = ModelVars()
+
+# create analysis settings
+list_analysis_vars = []
+for acceptor, id_ in [
+        (pyabc.UniformAcceptor(), "deterministic"),
+        (pyabc.UniformAcceptor(), "noisy_model"),
+        (pyabc.StochasticAcceptor(
+            temp_schemes=[
+                pyabc.acceptor.scheme_acceptance_rate,
+                pyabc.acceptor.scheme_decay]), "stochastic_acceptor")]:
+    list_analysis_vars.append(
+        AnalysisVars(
+            get_acceptor=lambda acceptor=acceptor: acceptor, id_=id_))
+
+# create tasks
+tasks = []
+for analysis_vars in list_analysis_vars:
+    tasks.append(Task.from_vars(analysis_vars, mv, 0))
+# overwrite deterministic setting
+tasks[0].model = mv.get_model()
+tasks[0].eps_min = 0.0
+
+# run
+for task in tasks:
+    task.execute()
---
+++ study_abc_noise/test/test_mrna.py
@@ -0,0 +1,5 @@
+from study_abc_noise.model import MRNATranscriptionModelVars as ModelVars
+
+mv = ModelVars()
+y_obs = mv.generate_data()
+print(y_obs)

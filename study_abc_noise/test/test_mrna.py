a6efe8ebd9b466f621d3a3e40422f84b67455946
yannikschaelte <yannik.schaelte@gmail.com>
2019-04-16 17:03:20 +0200

    modify test

--- study_abc_noise/test/test_mrna.py
+++ study_abc_noise/test/test_mrna.py
@@ -3,3 +3,5 @@ from study_abc_noise.model import MRNATranscriptionModelVars as ModelVars
 mv = ModelVars()
 y_obs = mv.generate_data()
 print(y_obs)
+mv.get_model()(mv.p_true)
+mv.get_model_noisy()(mv.p_true)

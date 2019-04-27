42c5b380cc304cebab3a67d137374c880ce01829
yannikschaelte <yannik.schaelte@gmail.com>
2019-04-27 06:10:23 +0200

    fix viz

--- study_abc_noise/acceptance_rate/script_viz.py
+++ study_abc_noise/acceptance_rate/script_viz.py
@@ -11,21 +11,25 @@ list_acc_rate_prior = val[2]
 list_acc_rate_prior_min = val[3]
 list_acc_rate_posterior = val[4]
 list_acc_rate_posterior_min = val[5]
+list_n_t = [4, 7, 10, 13, 16, 19, 22, 25, 28]
+print(val)
 
 _, ax = plt.subplots()
-ax.semilogy(list_n_t, list_pdf_max, label="$pdf_{max}$")
-ax.semilogy(list_n_t, list_pdf_max_min, label="$pdf_{opt}")
+ax.semilogy(list_n_t, list_pdf_max, 'x-', label="$pdf_{max}$")
+ax.semilogy(list_n_t, list_pdf_max_min, 'x-', label="$pdf_{opt}$")
 ax.set_xlabel("#Data")
 ax.set_ylabel("Maximum density")
+ax.legend()
 plt.savefig("pdf_maxs.png")
 
 
 _, ax = plt.subplots()
-ax.semilogy(list_n_t, list_acc_rate_prior, label="prior")
-ax.semilogy(list_n_t, list_acc_rate_prior_min, label="prior opt")
-ax.semilogy(list_n_t, list_acc_rate_posterior, label="posterior")
-ax.semilogy(list_n_t, list_acc_rate_posterior_min, label="posterior opt")
+ax.semilogy(list_n_t, list_acc_rate_prior, 'x-', label="prior")
+ax.semilogy(list_n_t, list_acc_rate_prior_min, 'x-', label="prior opt")
+ax.semilogy(list_n_t, list_acc_rate_posterior, 'x-', label="posterior")
+ax.semilogy(list_n_t, list_acc_rate_posterior_min, 'x-', label="posterior opt")
 ax.set_xlabel("#Data")
 ax.set_ylabel("Acceptance rate")
-plt.savefig("pdf_maxs.png")
+ax.legend()
+plt.savefig("acceptance_rates.png")
 

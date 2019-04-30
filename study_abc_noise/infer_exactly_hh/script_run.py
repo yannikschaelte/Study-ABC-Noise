79c32404661d366d12d27160956ee8e920cf00d8
yannikschaelte <yannik.schaelte@gmail.com>
2019-04-30 14:29:20 +0200

    try hh 10, 50, 1000 UUUUUUUU

--- study_abc_noise/infer_exactly_hh/script_run.py
+++ study_abc_noise/infer_exactly_hh/script_run.py
@@ -4,11 +4,17 @@ from study_abc_noise.model import HodgkinHuxleyModelVars as ModelVars
 from study_abc_noise.vars import AnalysisVars, Task
 
 
-mv = ModelVars(n_acc=100, n_t=100)
-
+mv = ModelVars(n_acc=500, n_t=1000)
 # create analysis settings
 list_analysis_vars = []
 for acceptor, id_ in [
+        (pyabc.StochasticAcceptor(
+            temp_schemes=[
+                pyabc.acceptor.scheme_acceptance_rate,
+                pyabc.acceptor.scheme_exponential_decay],
+            pdf_max_method=pyabc.acceptor.pdf_max_take_max_found
+            ),
+            "stochastic_acceptor_ada_c"),
         (pyabc.StochasticAcceptor(
             temp_schemes=[
                 pyabc.acceptor.scheme_acceptance_rate,
@@ -27,8 +33,8 @@ tasks = []
 for analysis_vars in list_analysis_vars:
     tasks.append(Task.from_vars(analysis_vars, mv))
 # overwrite deterministic setting
-tasks[1].model = mv.get_model()
-tasks[1].eps_min = 0.0
+tasks[2].model = mv.get_model()
+tasks[2].eps_min = 0.0
 
 # frun
 for task in tasks:
--- study_abc_noise/infer_exactly_hh/script_visualize.py
+++ study_abc_noise/infer_exactly_hh/script_visualize.py
@@ -23,13 +23,15 @@ for db_file in db_files:
 gt_par = h.get_population(t=-1).get_list()[0].parameter
 
 
-pyabc.visualization.plot_sample_numbers(histories, labels)
+pyabc.visualization.plot_sample_numbers(histories, labels, rotation=45)
 plt.savefig("samples.png")
 pyabc.visualization.plot_epsilons(histories, labels, scale="log10")
 plt.savefig("epsilons.png")
 for h, label in zip(histories, labels):
     pyabc.visualization.plot_histogram_matrix(h)
-    plt.savefig("hist_" + label + ".png")
-    df, w = h.get_distribution()
-    pyabc.visualization.plot_kde_matrix(df, w, refval=gt_par)#, limits=mv.limits)
-    plt.savefig("kde_" + label + ".png")
+    plt.savefig(f"hist_{label}.png")
+    for t in range(h.max_t + 1):
+        df, w = h.get_distribution(t=t)
+        pyabc.visualization.plot_kde_matrix(df, w, refval=gt_par, limits=mv.limits)
+        plt.savefig(f"kde_{label}_{t}.png")
+        plt.close()

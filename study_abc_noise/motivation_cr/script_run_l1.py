063f886966a34258f34e345d7a0646e6db6c0b39
yannikschaelte <yannik.schaelte@gmail.com>
2019-04-30 16:30:44 +0200

    add motivation_cr

---
+++ study_abc_noise/motivation_cr/script_run_l1.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pyabc
+from study_abc_noise.model import ConversionReaction1dModelVars as ModelVars
+from study_abc_noise.vars import AnalysisVars, Task
+
+
+mv = ModelVars()
+
+def l1(x, y):
+    return np.sum(np.abs(x['y'] - y['y']) / mv.noise_std)
+
+mv.get_distance = lambda: l1
+
+analysis_vars = AnalysisVars(
+    get_acceptor=lambda: pyabc.UniformAcceptor(),
+    id_="deterministic_l1")
+
+tasks = []
+tasks.append(Task.from_vars(analysis_vars, mv))
+tasks[0].model =mv.get_model()
+tasks[0].eps_min = 0.0
+
+# run
+for task in tasks:
+    task.execute()

c0ef8aaf25b33e5d99e9293e02737fbb18ec4ed8
yannikschaelte <yannik.schaelte@gmail.com>
2019-05-02 16:56:02 +0200

    use narrower boundaries in motivation, and add true posterior

--- study_abc_noise/motivation_cr/script_viz_true.py
+++ study_abc_noise/motivation_cr/script_viz_true.py
@@ -1,11 +1,21 @@
 import pyabc
 import os
 import matplotlib.pyplot as plt
-from study_abc_noise.model import ConversionReaction1dModelVars as ModelVars
+from study_abc_noise.model.conversion_reaction import *
+from study_abc_noise.read_pickle_file import read_pickle_file
 
 
-mv = ModelVars()
+mv = ConversionReaction1dModelVars()
 
+files = [f for f in os.listdir('data')]
+print(files)
+y_file = files[0]
+y_obs = read_pickle_file("data/" + y_file)
+print(y_obs)
+posterior_scaled = get_posterior_scaled_1d(mv, y_obs)
+
+xs = np.linspace(mv.limits['p0'][0], mv.limits['p0'][1], 200)
+true_vals = [posterior_scaled([x]) for x in xs]
 
 db_files = [f for f in os.listdir('.') if os.path.isfile(f) and "db_" in f]
 print(f"Using db files {db_files}")
@@ -20,7 +30,7 @@ for db_file in db_files:
     labels.append(id_)
 
 
-gt_par = h.get_population(t=-1).get_list()[0].parameter
+gt_par = h.get_population(t=0).get_list()[0].parameter
 
 
 pyabc.visualization.plot_sample_numbers(histories, labels)
@@ -28,12 +38,16 @@ plt.savefig("samples.png")
 pyabc.visualization.plot_epsilons(histories, labels, scale="log10")
 plt.savefig("epsilons.png")
 for h, label in zip(histories, labels):
-    pyabc.visualization.plot_histogram_1d(h, 'p0', xmin=mv.limits['p0'][0], xmax=mv.limits['p0'][1])
+    pyabc.visualization.plot_histogram_1d(h, 'p0', xmin=mv.limits['p0'][0], xmax=mv.limits['p0'][1], bins=40)
+    plt.plot(xs, true_vals, color='C4', label="True posterior")
+    plt.legend()
     plt.savefig("hist_" + label + ".png")
     for t in range(h.max_t + 1):
         df, w = h.get_distribution(t=t)
         pyabc.visualization.plot_kde_1d(df, w, 'p0', xmin=mv.limits['p0'][0], xmax=mv.limits['p0'][1])
-        plt.savefig(f"kde_{label}_{t}.png")
+        plt.plot(xs, true_vals, color='C4', label="True posterior")
+        plt.legend()
+        plt.savefig(f"kde_true_{label}_{t}.png")
         plt.close()
 pyabc.visualization.plot_effective_sample_sizes(histories, labels)
 plt.savefig("ess.png")
